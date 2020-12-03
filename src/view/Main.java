74cb45eaecfb569d93d80535f47099469cf37c7c
garret123h <garret12@uw.edu>
2020-12-02 15:56:56 -1000

    Implemented Main thread class

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,2 +1,13 @@
-package view;public class Main {
+package view;
+
+import controller.Simulator;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args){
+        try {
+            new Simulator();
+        } catch (IOException ex) { }
+    }
 }
