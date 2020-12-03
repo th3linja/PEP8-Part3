a65755b7a61d284f0ebfb9c8326474cb1889a667
garret123h <garret12@uw.edu>
2020-12-02 14:01:59 -1000

    Add Instruction domain object
    Why: Program needs a domain object for an Instruction.
    
    This change addresses the need by: Adding implemented Instruction object
    that utilizes polymorphism to create an accuarate representation of an
    Instruction.

---
+++ src/model/Instruction.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Instruction {
+    private String myRegisterSpecifier = "";
+
+    private String myOperand = "";
+
+    private String myAddressingMode = "";
+
+    private String myOpcode = "";
+
+    public Instruction(String opCode, String operand) {
+        this.myOpcode = opCode;
+        this.myOperand = operand;
+    }
+
+    public Instruction(String opCode, String registerSpec, String operand) {
+        this.myOpcode = opCode;
+        this.myRegisterSpecifier = registerSpec;
+        this.myOperand = operand;
+    }
+
+    public Instruction(String opCode, String registerSpec, String addressingMode, String operand) {
+        this.myOpcode = opCode;
+        this.myRegisterSpecifier = registerSpec;
+        this.myOperand = operand;
+        this.myAddressingMode = addressingMode;
+    }
+
+    @Override
+    public String toString() {
+        return this.myOpcode + this.myRegisterSpecifier + this.myAddressingMode + this.myOperand;
+    }
+
+    public String getAddressingMode() {
+        return (myAddressingMode == null)? "": myAddressingMode;
+    }
+
+    public String getOpcode() {
+        return (myOpcode == null)? "": myOpcode;
+    }
+    public String getOperand() {
+        return (myOperand == null)? "": myOperand;
+    }
+    public String getRegisterSpecifier() {
+        return (myRegisterSpecifier == null)? "": myRegisterSpecifier;
+    }
+}
