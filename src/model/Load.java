2080f9196afff969f7e986784182371540827e17
garret123h <garret12@uw.edu>
2020-12-09 10:45:42 -1000

    Implement Load execute method

--- src/model/Load.java
+++ src/model/Load.java
@@ -1,5 +1,8 @@
 package model;
 
+import utils.Converter;
+import utils.Transformer;
+
 public class Load extends Instruction{
 
     public Load(String opCode, String operand) {
@@ -16,6 +19,26 @@ public class Load extends Instruction{
 
     @Override
     public void execute(ControlUnit controlUnit) {
-
+        if (getRegisterSpecifier().contentEquals("0")) { //Accumulator
+            if (getAddressingMode().contentEquals("000")) { //immediate
+                controlUnit.setAR(Integer.parseInt(getOperand(), 2));
+            }
+            else if (getAddressingMode().contentEquals("001")) { //direct
+                int address = Converter.binToDecimal(getOperand());
+                String memBin = Integer.toBinaryString(
+                        Converter.hexToDecimal(controlUnit.memoryDump.getMemory(address)));
+                controlUnit.setAR(Integer.parseInt(memBin));
+            }
+        }else if (getRegisterSpecifier().contentEquals("1")) { //Index Reg
+            if (getAddressingMode().contentEquals("000")) { //immediate
+                controlUnit.setMyIndexRegister(Integer.parseInt(getOperand(), 2));
+            }
+            else if (getAddressingMode().contentEquals("001")) { //direct
+                int address = Converter.binToDecimal(getOperand());
+                String memBin = Integer.toBinaryString(
+                        Converter.hexToDecimal(controlUnit.memoryDump.getMemory(address)));
+                controlUnit.setMyIndexRegister(Integer.parseInt(memBin));
+            }
+        }
     }
 }
