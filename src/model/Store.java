490147381951a77c48222b4d7826ee7453cef03f
Ganghuan Huang <56037905+GH-H@users.noreply.github.com>
2020-12-10 12:01:18 -0800

    Update Store.java

--- src/model/Store.java
+++ src/model/Store.java
@@ -1,6 +1,8 @@
 package model;
 
-public class Store extends Instruction{
+import utils.Converter;
+
+public class Store extends Instruction {
 
     public Store(String opCode, String operand) {
         super(opCode, operand);
@@ -16,6 +18,13 @@ public class Store extends Instruction{
 
     @Override
     public void execute(ControlUnit controlUnit) {
+        String hexAddress = Converter.binToHex(super.getOperand());
+
+        if (super.getRegisterSpecifier().equals("0")) {
+            controlUnit.memoryDump.setMemory(hexAddress, controlUnit.getAR());
+        } else {
+            controlUnit.memoryDump.setMemory(hexAddress, controlUnit.getMyIndexRegister());
+        }
 
     }
 }
