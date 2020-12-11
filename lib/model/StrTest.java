9355e5f66a7c3852f39b4233fbc40385fdf46f89
Ganghuan Huang <56037905+GH-H@users.noreply.github.com>
2020-12-10 19:48:22 -0800

    format change

--- lib/model/StrTest.java
+++ lib/model/StrTest.java
@@ -7,11 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class StrTest {
 
     @Test
-    void testStr(){
+    void testStr() {
         ControlUnit controlUnit = new ControlUnit();
         controlUnit.setAR(10);
-        Instruction str = new Str("1110","0","001","0000000000000001");
+        Instruction str = new Str("1110", "0", "001", "0000000000000001");
         str.execute(controlUnit);
-        assertEquals("0A",controlUnit.memoryDump.getMemory(1));
+        assertEquals("0A", controlUnit.memoryDump.getMemory(1));
     }
 }
